9b34dbca4c5314c7dafbad81cf8f68ae30bf52f7
henrique.alonso <henrique.alonso>
2024-11-06 01:09:58 -0300

    Adequação da arquitetura e implementação do AWS SQS

--- src/Gateways/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Gateways/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Infra.DependencyInjection;
+﻿using Amazon.SQS;
+using Core.Infra.MessageBroker;
+using Core.Infra.MessageBroker.DependencyInjection;
+using Gateways.Dtos.Events;
+using Infra.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,13 +11,30 @@ namespace Gateways.DependencyInjection
     [ExcludeFromCodeCoverage]
     public static class ServiceCollectionExtensions
     {
-        public static void AddGatewayDependencyServices(this IServiceCollection services, string connectionString)
+        public static void AddGatewayDependencyServices(this IServiceCollection services, string connectionString, Queues queues)
         {
             services.AddScoped<IClienteGateway, ClienteGateway>();
             services.AddScoped<IProdutoGateway, ProdutoGateway>();
             services.AddScoped<IFuncionarioGateway, FuncionarioGateway>();
 
             services.AddInfraDependencyServices(connectionString);
+
+            // AWS SQS
+            services.AddAwsSqsMessageBroker();
+
+            services.AddSingleton<ISqsService<ProdutoCriadoEvent>>(provider => new SqsService<ProdutoCriadoEvent>(provider.GetRequiredService<IAmazonSQS>(), queues.QueueProdutoCriadoEvent));
+            services.AddSingleton<ISqsService<ProdutoAtualizadoEvent>>(provider => new SqsService<ProdutoAtualizadoEvent>(provider.GetRequiredService<IAmazonSQS>(), queues.QueueProdutoAtualizadoEvent));
+            services.AddSingleton<ISqsService<ProdutoExcluidoEvent>>(provider => new SqsService<ProdutoExcluidoEvent>(provider.GetRequiredService<IAmazonSQS>(), queues.QueueProdutoExcluidoEvent));
+            services.AddSingleton<ISqsService<ClienteCriadoEvent>>(provider => new SqsService<ClienteCriadoEvent>(provider.GetRequiredService<IAmazonSQS>(), queues.QueueClienteCriadoEvent));
         }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public record Queues
+    {
+        public string QueueProdutoCriadoEvent { get; set; } = string.Empty;
+        public string QueueProdutoAtualizadoEvent { get; set; } = string.Empty;
+        public string QueueProdutoExcluidoEvent { get; set; } = string.Empty;
+        public string QueueClienteCriadoEvent { get; set; } = string.Empty;
     }
 }
